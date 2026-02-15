730fe03752600a5d92cddcb41fd73a8fab4773ef
afriedb <friedbergapgc@hotmail.com>
2026-02-15 13:41:13 +0000

    Initial weightloss prototype

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    button.classList.add('clicked');
+    setTimeout(() => button.classList.remove('clicked'), 200);
+  });
+});
